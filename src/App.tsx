b667c08172b654f91ef3404e11aa58c86d98b3ed
Barbora Lendacka <borka34@gmail.com>
2025-09-17 22:35:55 +0200

    Add AOS refresh component to App so animations initialize correctly after navigation in Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import Routes from './Routes';
 import Page from './components/Page';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+const AOSRouteRefresher = (): null => {
+  const location = useLocation();
+  React.useEffect(() => {
+    try {
+      AOS.refresh();
+    } catch {
+      /* noop */
+    }
+  }, [location.pathname]);
+  return null;
+};
 
 const App = (): JSX.Element => {
   return (
     <Page>
       <BrowserRouter>
         <Routes />
+        <AOSRouteRefresher />
       </BrowserRouter>
     </Page>
   );
