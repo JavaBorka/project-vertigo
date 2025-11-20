89a785a5e9a933b63d44309fe6bad83f2eaae26f
Barbora Lendacka <borka34@gmail.com>
2025-11-20 19:44:01 +0100

    Integrate Firebase with env API key and fetch initial data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import Routes from './Routes';
 import Page from './components/Page';
@@ -7,6 +7,27 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getRemoteConfig } from "firebase/remote-config";
+import { getValue } from "firebase/remote-config";
+import { fetchAndActivate } from "firebase/remote-config";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY,
+  authDomain: "test-project-aac6d.firebaseapp.com",
+  projectId: "test-project-aac6d",
+  storageBucket: "test-project-aac6d.firebasestorage.app",
+  messagingSenderId: "921750506680",
+  appId: "1:921750506680:web:07811cd9f82d263092384d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const remoteConfig = getRemoteConfig(app);
 
 const AOSRouteRefresher = (): null => {
   const location = useLocation();
@@ -20,7 +41,25 @@ const AOSRouteRefresher = (): null => {
   return null;
 };
 
+
 const App = (): JSX.Element => {
+  
+  // const books = getValue(remoteConfig, "BOOKS_JSON");
+  // console.log(books)
+
+  useEffect(()=> {
+    fetchAndActivate(remoteConfig)
+  .then(() => {
+    const books = getValue(remoteConfig, "BOOKS_JSON").asString();
+    const data = JSON.parse(books);
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+  },[]
+)
+
   return (
     <Page>
       <BrowserRouter>
