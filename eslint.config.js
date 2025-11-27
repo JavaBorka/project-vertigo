86349e41084b8c349780a037173810697c3fc933
Barbora Lendacka <borka34@gmail.com>
2025-11-27 12:38:08 +0100

    Fix ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,61 @@
-/* eslint-disable no-undef */
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
+// ESLint 9+ flat config
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...(tsPlugin.configs?.recommended?.rules ?? {}),
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'only-multiline'],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    plugins: {
+      react: reactPlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'only-multiline'],
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'only-multiline'],
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+  {
+    ignores: ['node_modules/', 'dist/', 'build/'],
   },
-};
+];
