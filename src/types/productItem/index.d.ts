cf2ec25074eab8b2b0572f4ffd8596ffecf75c62
Barbora Lendacka <borka34@gmail.com>
2026-01-11 12:48:06 +0100

    Add typescript

--- src/types/productItem/index.d.ts
+++ src/types/productItem/index.d.ts
@@ -17,3 +17,20 @@ interface CardItemProps {
 interface ProductGridProps {
   items: ProductItem[];
 }
+
+export type AuthorItem = {
+  id: string;
+  media: string;
+  author: string;
+  title: string;
+  country: string;
+  label: string;
+};
+
+interface AuthorCardItemProps {
+  item: AuthorItem;
+}
+
+interface AuthorGridProps {
+  items: AuthorItem[];
+}
