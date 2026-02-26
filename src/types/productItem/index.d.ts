efee3c3e264c9a5eb29b32746c3b36e856fffb22
Barbora Lendacka <borka34@gmail.com>
2026-02-26 01:40:56 +0100

    Replace author types from products folder to authors folder

--- src/types/productItem/index.d.ts
+++ src/types/productItem/index.d.ts
@@ -19,20 +19,3 @@ interface CardItemProps {
 interface ProductGridProps {
   items: ProductItem[];
 }
-
-export type AuthorItem = {
-  id: string;
-  media: string;
-  author: string;
-  title: string;
-  country: string;
-  label: string;
-};
-
-interface AuthorCardItemProps {
-  item: AuthorItem;
-}
-
-interface AuthorGridProps {
-  items: AuthorItem[];
-}
