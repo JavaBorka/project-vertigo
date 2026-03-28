29419cfbf775043e16006d35a06c7fb1f080156a
Barbora Lendacka <borka34@gmail.com>
2026-03-28 18:36:28 +0100

    Set light theme across the app

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,40 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import CssBaseline from '@mui/material/CssBaseline';
 import getTheme from 'theme';
-
-export const useDarkMode = (): [string, () => void, boolean] => {
-  const [themeMode, setTheme] = useState('light');
-  const [mountedComponent, setMountedComponent] = useState(false);
-
-  const setMode = (mode: string) => {
-    try {
-      window.localStorage.setItem('themeMode', mode);
-    } catch {
-      /* do nothing */
-    }
-
-    setTheme(mode);
-  };
-
-  const themeToggler = (): void => {
-    themeMode === 'light' ? setMode('dark') : setMode('light');
-  };
-
-  useEffect(() => {
-    try {
-      const localTheme = window.localStorage.getItem('themeMode');
-      localTheme ? setTheme(localTheme) : setMode('light');
-    } catch {
-      setMode('light');
-    }
-
-    setMountedComponent(true);
-  }, []);
-
-  return [themeMode, themeToggler, mountedComponent];
-};
 
 interface Props {
   children: React.ReactNode;
@@ -47,15 +15,27 @@ export default function Page({ children }: Props) {
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
+
+    try {
+      window.localStorage.removeItem('themeMode');
+    } catch {
+      /* do nothing */
+    }
   }, []);
 
-  const [themeMode] = useDarkMode();
-
   return (
-    <ThemeProvider theme={getTheme(themeMode)}>
+    <ThemeProvider theme={getTheme('light')}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <Paper elevation={0}>{children}</Paper>
+      <Paper
+        elevation={0}
+        sx={{
+          minHeight: '100vh',
+          backgroundColor: 'background.default',
+        }}
+      >
+        {children}
+      </Paper>
     </ThemeProvider>
   );
 }
